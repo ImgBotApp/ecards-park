2c518e77be09ea2be8838d7fc014bb038785cd29
Ziga Vukcevic <ziga@be-codified.com>
2017-12-06 15:07:48 +0100

    Add header & footer

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Article = mongoose.model('Article');
+// const Article = mongoose.model('Article');
 
 module.exports = (app) => {
   app.use('/', router);
 };
 
 router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Some new page'
+  });
+
+  /*
   Article.find((err, articles) => {
     if (err) return next(err);
     res.render('index', {
       title: 'Generator-Express MVC',
       articles: articles
     });
+  });
+  */
+});
+
+router.get('/test', (req, res, next) => {
+  res.render('index', {
+    title: 'Test page'
   });
 });
