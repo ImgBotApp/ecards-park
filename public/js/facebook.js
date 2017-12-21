0158ea5432f953001b806e418db100c4b9104f41
Ziga Vukcevic <ziga@be-codified.com>
2017-12-21 16:57:52 +0100

    Dist

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,1 +1,1 @@
-"use strict";!function(e,t,n){var s,c=e.getElementsByTagName("script")[0];e.getElementById(n)||((s=e.createElement("script")).id=n,s.src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=170073533599934",c.parentNode.insertBefore(s,c))}(document,0,"facebook-jssdk");
+"use strict";!function(e,t,n){var s,c=e.getElementsByTagName("script")[0];e.getElementById(n)||((s=e.createElement("script")).id=n,s.src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=347207519086319",c.parentNode.insertBefore(s,c))}(document,0,"facebook-jssdk");
